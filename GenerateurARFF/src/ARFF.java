0371ee4642f16634a8b94406c8f9db6b47d300ba
llafon <llafon@w8.etu.info-ufr.univ-montp2.fr>
2015-04-02 11:00:08 +0200

    toto2

--- GenerateurARFF/src/ARFF.java
+++ GenerateurARFF/src/ARFF.java
@@ -10,44 +10,64 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 
 public class ARFF {
 
 	public String  arffContent;
 	
-	public ARFF(String filePath, String nomClasse, String targetPath){
-		arffContent = "\n\n@relation series";
+	public ARFF(String foldersPath, String targetPath){
+		arffContent = "% Ceci représente la classification de séries en fonction de leur genre.";
+		arffContent += "\n\n@relation series";
 		arffContent += "\n\n@attribute nom_fichier string";
 		arffContent += "\n@attribute contenu string";
 		arffContent += "\n@attribute classe {animation, crime, medical}";
 		arffContent += "\n\n@data";
 		
+		File file = new File("");
+		ArrayList<String> folderNames = new ArrayList<String>();
+		folderNames.add("animation");
+		folderNames.add("crime");
+		folderNames.add("medical");
+		
+		for(String folderName: folderNames){
+			File[] files= getListOfFiles(foldersPath + "/" + folderName);
+			for(File f: files){
+				try {
+					//Path p = Paths.get(filePath);
+					String fileName = f.getAbsolutePath();
+					String fileContent = readFile(fileName, StandardCharsets.UTF_8);
+					arffContent += "\n\'" + fileName.split("/")[fileName.split("/").length - 1] + "\',\'"+ fileContent.replace("\'", "\\'").replace("\n", "") + "\'," + folderName;
+					System.out.println(arffContent);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		FileWriter fw;
 		try {
-			Path p = Paths.get(filePath);
-			String fileName = p.getFileName().toString();
 			
-			String fileContent = readFile(filePath, StandardCharsets.UTF_8);
-			arffContent += "\n" + fileName + ",\""+ fileContent.replace("\"", "").replace("\n", "") + "\"," + nomClasse;
-			System.out.println(arffContent);
-			
-			File file = new File(targetPath);
+			file = new File(targetPath);
 			 
 			// if file doesnt exists, then create it
 			if (!file.exists()) {
 				file.createNewFile();
-			}
- 
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			} 
+			
+			fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
 			bw.write(arffContent);
 			bw.close();
- 
+			
 			System.out.println("Done");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 	
 	static String readFile(String path, Charset encoding) 
@@ -56,4 +76,19 @@ public class ARFF {
 			  byte[] encoded = Files.readAllBytes(Paths.get(path));
 			  return new String(encoded, encoding);
 			}
+	
+	public static File[] getListOfFiles(String folderPath){
+		File folder = new File(folderPath);
+		File[] listOfFiles = folder.listFiles();
+		System.out.println(folderPath);
+	    for (int i = 0; i < listOfFiles.length; i++) {
+	      if (listOfFiles[i].isFile()) {
+	        System.out.println("File " + listOfFiles[i].getName());
+	      } else if (listOfFiles[i].isDirectory()) {
+	        System.out.println("Directory " + listOfFiles[i].getName());
+	      }
+	    }	
+	    
+	    return listOfFiles;
+	}
 }
