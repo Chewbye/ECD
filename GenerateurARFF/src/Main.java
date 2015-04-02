3b1eda89ba7ee424b6f4d0becc3399047158e42d
Axel347 <axelgohan34@gmail.com>
2015-04-03 00:05:43 +0200

    Mise en place des textes lemmatisés et du fichier ARFF correspondant

--- GenerateurARFF/src/Main.java
+++ GenerateurARFF/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	public static void main(String[] args){
 		String projectPath = "/auto_home/llafon/Cours/semestre8/ECD/ECD/corpus";
 		ARFF arff = new ARFF(projectPath, projectPath + "/result.arff");
+
 	}
 }
